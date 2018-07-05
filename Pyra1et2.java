e42e67336398b1dbe11c3eaa8bf2c92543b0dcea
Alice Gaudon <alice@gaudon.pro>
2018-07-05 15:05:41 +0200

    Renamed some variables to add more meaning and precision

--- Pyra1et2.java
+++ Pyra1et2.java
@@ -5,55 +5,55 @@ import java.util.Scanner;
 
 public class Pyra1et2 {
 
-    private static int lines = 0;
-    private static String direction = "";
+    private static int lineCount = 0;
+    private static String directionInputStr = "";
 
     public static void main(String[] args) {
         // Prompt the user to select the pyramid size and direction
-        try (Scanner clavier = new Scanner(System.in)) {
+        try (Scanner input = new Scanner(System.in)) {
             System.out.println("Entrer un entier :");
-            lines = clavier.nextInt();
+            lineCount = input.nextInt();
 
             System.out.println("Choisir le sens de la pyramide (up or down) :");
-            clavier.nextLine();
-            direction = clavier.nextLine();
+            input.nextLine();
+            directionInputStr = input.nextLine();
         } catch (InputMismatchException e) {
             System.out.println("Ce n'est pas un valeur possible.");
             return;
         }
 
-        Direction directionEnum = Direction.fromName(direction);
+        Direction direction = Direction.fromName(directionInputStr);
 
-        if (directionEnum == null) {
+        if (direction == null) {
             System.out.println("Choix non valide.");
             return;
         }
 
         // Draw the pyramid according to the requested size and direction
-        for (int i = 0; i < lines; i++) {
-            int value;
-            switch (directionEnum) {
+        for (int i = 0; i < lineCount; i++) {
+            int lineIndex;
+            switch (direction) {
                 case UP:
-                    value = i;
+                    lineIndex = i;
                     break;
                 case DOWN:
-                    value = lines - 1 - i;
+                    lineIndex = lineCount - 1 - i;
                     break;
                 default:
-                    throw new UnsupportedOperationException("The " + directionEnum + " direction is not supported");
+                    throw new UnsupportedOperationException("The " + direction + " direction is not supported");
             }
-            drawLine(value);
+            drawLine(lineIndex);
         }
     }
 
     /**
      * Draw one line of the pyramid
      *
-     * @param currentLine The index of the line from {@code 0} to {@code size - 1}
+     * @param lineIndex The index of the line from {@code 0} to {@code size - 1}
      */
-    public static void drawLine(int currentLine) {
-        for (int e = (lines - 1) - (currentLine + 1); e >= 0; e--) System.out.print(" ");
-        for (int e = 2 * currentLine; e >= 0; e--) System.out.print("*");
+    public static void drawLine(int lineIndex) {
+        for (int e = (lineCount - 1) - (lineIndex + 1); e >= 0; e--) System.out.print(" ");
+        for (int e = 2 * lineIndex; e >= 0; e--) System.out.print("*");
         System.out.println();
     }
 
